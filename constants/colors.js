55771bbaf60510cef6135c0aa0d881f9001ae8cf
Leonardo <leonardo38361@gmail.com>
2024-01-18 21:41:43 -0300

    Add 'constants' folder with configuration files

---
+++ constants/colors.js
@@ -0,0 +1,27 @@
+export const colors = [
+    "#FF0000", // Rojo 
+    "#33CC33", // Verde
+    "#0000FF", // Azul
+    "#CCCC00", // Amarillo
+    "#FF00FF", // Magenta
+    "#00CCFF", // Cian
+    "#FF8000", // Naranja
+    "#8000FF", // Púrpura
+    "#0080FF", // Azul claro
+    "#800080", // Morado    
+    "#808080", // Gris    
+    "#800000", // Marrón oscuro
+    "#008000", // Verde oscuro
+    "#000080", // Azul oscuro
+    "#808000", // Ocre
+    "#FF0080", // Rosa
+    "#008080", // Turquesa
+    "#ccccff", // Violeta claro
+    "#FFC0CB", // Rosa claro
+    "#FFD700", // Oro
+    "#A52A2A", // Marrón
+    "#FFA500", // Naranja claro
+    "#B3FF66", // Verde claro
+    "#DB7093", // Rosado
+    "#2F4F4F"  // Gris oscuro
+];
---
+++ constants/strings.js
@@ -0,0 +1,5 @@
+export const FormValidations = {
+    REQUIRED: 'Este campo es obligatorio.',
+    GEOLOCATION: 'Por favor, ingresa una geolocalización válida en el formato de ejemplo -32.88674234061537, -68.85978536314231',
+    
+  }
