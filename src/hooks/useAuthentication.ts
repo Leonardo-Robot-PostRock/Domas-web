a68ba07590b3e2644cdde8454a61d0b40163883a
Leonardo <leonardo38361@gmail.com>
2024-01-24 18:43:28 -0300

    Modifications to layout.tsx, page.tsx, and added useAuthentication.ts with TypeScript.

--- src/hooks/useAuthentication.ts
+++ src/hooks/useAuthentication.ts
@@ -1,3 +1,4 @@
+"use client";
 import { useEffect, useState } from "react";
 
 //Dicebear imports
@@ -5,16 +6,16 @@ import { thumbs } from "@dicebear/collection";
 import { createAvatar } from "@dicebear/core";
 
 //Types
-import { AuthenticationHookResult } from "@/types/dicebear/authHookResult";
 import { User } from "@/types/dicebear/user";
 import { Role } from "@/types/dicebear/role";
+import { AuthenticationHookResult } from "@/types/authTypes/authHookResult";
 
 export const useAuthentication = (): AuthenticationHookResult => {
   const [userInfo, setUserInfo] = useState<User | null>(null);
   const [userRoles, setUserRoles] = useState<string[]>([]);
 
   useEffect(() => {
-    const userJson = localStorage.getItem('user');
+    const userJson = localStorage.getItem("user");
 
     if (userJson) {
       const user: User = JSON.parse(userJson);
@@ -30,7 +31,7 @@ export const useAuthentication = (): AuthenticationHookResult => {
 
       if (user.roles) {
         const roles: Role[] = user.roles;
-        setUserRoles(roles.map(item => item.name));
+        setUserRoles(roles.map((item) => item.name));
       }
     }
   }, []);
