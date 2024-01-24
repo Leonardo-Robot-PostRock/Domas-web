0d6f082159667e7aa497a3f985f30603952356ed
Leonardo <leonardo38361@gmail.com>
2024-01-24 19:25:19 -0300

    [ADD] new NavMenuItem component

---
+++ components/NavbarItems/NavMenuItem.tsx
@@ -0,0 +1,29 @@
+import { NavMenuItemProps } from "@/types/NavMenuItemProps/navMenuItemProps";
+
+import { Flex, Text } from "@chakra-ui/react";
+import Link from "next/link";
+
+const NavMenuItem: React.FC<NavMenuItemProps> = ({ children }) => (
+  <Link rounded={"md"} href={children.link} passHref>
+    <Flex
+      alignItems={"center"}
+      bg={children.selected ? "#2F3A44" : "white"}
+      color={children.selected ? "white" : "#2F3A44"}
+      cursor={"pointer"}
+      gap={2}
+      m={1}
+      p={2}
+      borderRadius={"20vh"}
+      transition={".3s"}
+      _hover={{
+        bg: "#2F3A44",
+        color: "white",
+      }}
+    >
+      {children.icon ? children.icon : null}
+      <Text>{children.title}</Text>
+    </Flex>
+  </Link>
+);
+
+export default NavMenuItem;
