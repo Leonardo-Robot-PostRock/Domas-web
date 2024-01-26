46d035794bc3c8ababc9bb8dcdf9b9dd9fb84691
Leonardo <leonardo38361@gmail.com>
2024-01-25 22:54:01 -0300

    [UPDATE] layout for Next.js v13+

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,11 +1,11 @@
-"use client";
+'use client';
 
-import { useRouter, usePathname } from "next/navigation";
-import Cookies from "js-cookie";
+import { useRouter, usePathname } from 'next/navigation';
+import Cookies from 'js-cookie';
 
-import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 
-import Navbar from "./navbar";
+import Navbar from './navbar';
 
 export type Props = {
   params: { id: string };
@@ -15,59 +15,47 @@ export type Props = {
 export async function generateMetadata({ params, searchParams }: Props) {
   const router: AppRouterInstance = useRouter();
 
-  console.log("ver", params);
+  console.log('ver', params);
 
   const pathname: string | null = usePathname();
 
   const cookiesAuth = Cookies.get();
 
-  if (cookiesAuth && !cookiesAuth.auth_service && pathname !== "/login") {
-    router.push("/login");
+  if (cookiesAuth && !cookiesAuth.auth_service && pathname !== '/login') {
+    router.push('/login');
   }
 
-  const pathnameSplit: string[] | undefined = pathname?.split("/");
-  const titlePathname: string | undefined = pathnameSplit
-    ? pathnameSplit[pathnameSplit.length - 1]
-    : "";
+  const pathnameSplit: string[] | undefined = pathname?.split('/');
+  const titlePathname: string | undefined = pathnameSplit ? pathnameSplit[pathnameSplit.length - 1] : '';
 
   return {
     title:
-      process.env.APP_ENV?.toUpperCase() == "PROD"
-        ? process.env.APP_NAME + "|" + titlePathname || "HOME"
-        : process.env.APP_ENV?.toUpperCase() == "DEV"
-          ? "LOCAL DUMAS"
-          : "TEST DUMAS",
+      process.env.APP_ENV?.toUpperCase() == 'PROD'
+        ? process.env.APP_NAME + '|' + titlePathname || 'HOME'
+        : process.env.APP_ENV?.toUpperCase() == 'DEV'
+          ? 'LOCAL DUMAS'
+          : 'TEST DUMAS',
     icons: {
-      icon:
-        process.env.APP_ENV?.toUpperCase() == "PROD"
-          ? "/favicon.ico"
-          : "/faviconLocal.ico",
+      icon: process.env.APP_ENV?.toUpperCase() == 'PROD' ? '/favicon.ico' : '/faviconLocal.ico',
     },
     description: process.env.APP_NAME,
     openGraph: {
-      title: "Do+",
-      images:
-        process.env.APP_ENV?.toUpperCase() == "PROD"
-          ? "/logo.svg"
-          : "/logoLocal.svg",
+      title: 'Do+',
+      images: process.env.APP_ENV?.toUpperCase() == 'PROD' ? '/logo.svg' : '/logoLocal.svg',
     },
   };
 }
 
 interface LayoutComponent {
   children: React.ReactNode;
-  props: Props;
 }
 
-export default function Layout({
-  children,
-  props: { params, searchParams },
-}: LayoutComponent) {
-  console.log("params", params);
+export default function Layout({ children }: LayoutComponent) {
+  const pathname: string | null = usePathname();
 
   return (
     <>
-      <nav>{titlePathname != "LOGIN" && <Navbar />}</nav>
+      <nav>{pathname !== 'LOGIN' && <Navbar />}</nav>
       <main>{children}</main>
     </>
   );
