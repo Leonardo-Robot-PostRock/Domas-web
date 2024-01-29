8b4821ff75add1d728029c47e582cb4afb8cf3f3
Leonardo <leonardo38361@gmail.com>
2024-01-29 12:42:43 -0300

    Provider is added according to documentation for Next

---
+++ src/app/providers.tsx
@@ -0,0 +1,8 @@
+'use client';
+import theme from '@/styles/global';
+
+import { ChakraProvider } from '@chakra-ui/react';
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+}
