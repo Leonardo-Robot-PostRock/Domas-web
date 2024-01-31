b43d0cf6aadae52c8b05946f41ff371ff27fd6f8
Leonardo <leonardo38361@gmail.com>
2024-01-31 11:28:01 -0300

    restored method onSubmit

--- src/app/(dashboard)/login/page.tsx
+++ src/app/(dashboard)/login/page.tsx
@@ -20,6 +20,7 @@ import {
 import { useToast } from '@chakra-ui/react';
 
 import { colord } from 'colord';
+import axios from 'axios';
 
 import { Inputs } from '@/types/Form/inputs';
 import Cookies from 'js-cookie';
@@ -39,51 +40,41 @@ export default function SignInPage() {
     handleSubmit,
   } = useForm<Inputs>();
 
-  const onSubmit: SubmitHandler<Inputs> = async (data: Inputs) => {
+  const onSubmit: SubmitHandler<Inputs> = (data: Inputs) => {
     Cookies.remove('auth_service');
     setSubmitIsLoading(true);
+    axios
+      .post('/api/login', data)
+      .then((res) => {
+        console.log('RES', res);
 
-    try {
-      const response = await fetch('/api/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json', // Especifica el tipo de contenido
-        },
-        body: JSON.stringify(data), // Convierte los datos a JSON antes de enviarlos
-      });
+        const { user } = res.data;
+        const { auth_service, Domain, Path } = res.data.sessionCookie;
 
-      if (response.ok) {
-        // Si la respuesta es exitosa (código de estado 2xx)
-        const res = await response.json(); // Convierte la respuesta a JSON
+        console.log('USE', user);
 
-        console.log(res);
-
-        // Extrae los datos necesarios de la respuesta y realiza las acciones correspondientes
-        const { user, session_cookie } = res;
-        const { auth_service, Domain, Path } = session_cookie;
         localStorage.setItem('user', JSON.stringify(user));
-        Cookies.set('auth_service', auth_service, { expires: 0.5 });
+
+        Cookies.set('auth_service', auth_service, {
+          expires: 0.5,
+        });
 
         router.push('/');
-      } else {
-        // Si la respuesta no es exitosa, lanza un error
-        throw new Error('La solicitud no fue exitosa');
-      }
-    } catch (err) {
-      console.error(err);
+      })
+      .catch((err) => {
+        console.error(err);
 
-      toast({
-        position: 'top',
-        isClosable: true,
-        render: () => (
-          <Box bg={'#282828'} color={'white'} p={4} rounded={'xl'}>
-            Las credenciales no pertenecen a ningún usuario de DO+
-          </Box>
-        ),
-      });
-    } finally {
-      setSubmitIsLoading(false);
-    }
+        toast({
+          position: 'top',
+          isClosable: true,
+          render: () => (
+            <Box bg={'#282828'} color={'white'} p={4} rounded={'xl'}>
+              Las credenciales no pertenecen a ningún usuario de DO+
+            </Box>
+          ),
+        });
+      })
+      .finally(() => setSubmitIsLoading(false));
   };
 
   return (
