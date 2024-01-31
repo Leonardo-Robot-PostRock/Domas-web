c82eaae677794abe8aeec7eaadbf2d416c831232
Leonardo <leonardo38361@gmail.com>
2024-01-31 11:20:26 -0300

    Handle Axios errors and parse session cookie in POST request

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,68 +1,60 @@
-interface Props extends Request {
-  username: string;
-  password: string;
-}
+import isAxiosError from '@/utils/AxiosError';
 
-interface ErrorMessage {
-  message: string;
-}
+const axios = require('axios').default;
 
-function isErrorMessage(error: any): error is ErrorMessage {
-  return typeof error?.message === 'string';
+interface Cookie {
+  [key: string]: string;
 }
 
 export async function POST(request: Request) {
-  const body: Props = await request.json();
+  const body = await request.json();
 
   const { username, password } = body;
 
   try {
-    const response = await fetch(`${process.env.AUTH_BASE_URL}/auth/login`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        username,
-        password,
-      }),
-    });
-
-    if (!response.ok) {
-      throw new Error('Failed to login');
-    }
-
-    const session_cookie = response.headers.get('set-cookie');
-    if (!session_cookie) {
-      throw new Error('Session cookie not found');
-    }
-
-    const cookieParts = session_cookie.split(';');
-
-    const obj: { [key: string]: string } = {};
-
-    cookieParts.forEach((part: string) => {
-      const [key, value] = part.split('=');
-      if (key && value) {
-        obj[key.trim()] = value.trim();
-      }
-    });
-
-    console.log(obj);
-
-    const responseBody = {
+    const axiosResponse = await axios.post(`${process.env.AUTH_BASE_URL}/auth/login`, {
       username,
       password,
-      session_cookie: obj,
+    });
+
+    console.log('axiosResponse', axiosResponse);
+
+    const session_cookie = axiosResponse.headers['set-cookie'][0];
+    const cookieParts = session_cookie.split(';');
+
+    const obj = cookieParts.reduce((accumulator: Cookie, currentValue: string) => {
+      let [key, value] = currentValue.split('=');
+      key = key.replaceAll(' ', '');
+      accumulator[key] = value;
+      return accumulator;
+    }, {});
+
+    const responseBody = {
+      user: axiosResponse.data,
+      sessionCookie: obj,
     };
 
-    console.log(responseBody);
-
     return new Response(JSON.stringify(responseBody), { status: 200, headers: { 'Content-Type': 'application/json' } });
-  } catch (error) {
-    if (isErrorMessage(error)) {
-      return new Response(JSON.stringify({ error: error.message }), {
-        status: 500,
+  } catch (error: unknown) {
+    console.error('Error:', error);
+    if (isAxiosError(error)) {
+      if (error.response) {
+        // The request was made and the server responded with a status code that falls out of the range of 2xx
+        console.log('axios error.response.data', error.response.data);
+        return new Response(JSON.stringify(error.response.data), {
+          status: error.response.status,
+          headers: { 'Content-Type': 'application/json' },
+        });
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.log('axios error.request', error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('axios error.message', error.message);
+      }
+
+      return new Response(JSON.stringify({ message: 'Credenciales incorrectas' }), {
+        status: 400,
         headers: { 'Content-Type': 'application/json' },
       });
     }
