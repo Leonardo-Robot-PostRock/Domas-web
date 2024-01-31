d20e1ca4bd8f1e7927f9063d0ced1feb8e9b7fed
Leonardo <leonardo38361@gmail.com>
2024-01-31 11:25:28 -0300

    Add type guard function to check for AxiosError

--- utils/AxiosError.ts
+++ utils/AxiosError.ts
@@ -1,3 +1,5 @@
+import { AxiosError } from 'axios';
+
 export default function isAxiosError(candidate: unknown): candidate is AxiosError {
   if (candidate && typeof candidate === 'object' && 'isAxiosError' in candidate) {
     return true;
