2569f570b881dd096c5fdef4ec1c1619103b8bfd
Leonardo <leonardo38361@gmail.com>
2024-02-01 11:25:17 -0300

    add SubmitButton component

---
+++ components/buttons/SubmitButton.tsx
@@ -0,0 +1,25 @@
+import { Button } from '@chakra-ui/react';
+import { colord } from 'colord';
+import { useGradientOfTheDay } from '@/hooks/useGradientOfTheDay';
+
+interface Props {
+  onSubmit: () => void;
+  isLoading: boolean;
+}
+
+export const SubmitButton = ({ onSubmit, isLoading }: Props) => {
+  const { gradientOfTheDay } = useGradientOfTheDay();
+
+  return (
+    <Button
+      bg={gradientOfTheDay.split(',')[0]}
+      onClick={onSubmit}
+      isLoading={isLoading}
+      _hover={{
+        bg: colord(gradientOfTheDay.split(',')[0]).darken(0.05).toHex(),
+      }}
+    >
+      Ingresar a DO+
+    </Button>
+  );
+};
