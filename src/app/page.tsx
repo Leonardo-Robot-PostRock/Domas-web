15cc987a5504ac4f52d99096e6cfb60bc73ea9ad
Leonardo <leonardo38361@gmail.com>
2024-02-01 17:50:21 -0300

    It adds measurements for the logo and adds metadata.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import LoginForm from '@/components/LoginForm';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
+  metadataBase: new URL(process.env.API_URL as string),
   title: 'Bienvenidos a Do+',
   description: 'Página de incio de sesión',
   icons: {
@@ -9,7 +10,11 @@ export const metadata: Metadata = {
   },
   openGraph: {
     title: 'Do+',
-    images: process.env.APP_ENV!.toUpperCase() === 'PROD' ? '/logo.svg' : '/logoLocal.svg',
+    images: {
+      url: process.env.APP_ENV!.toUpperCase() === 'PROD' ? '/logo.svg' : '/logoLocal.svg',
+      width: 800,
+      height: 600,
+    },
   },
 };
 
