1bb96b0da663935bf4a998f723d10158ad69a353
Leonardo <leonardo38361@gmail.com>
2024-02-01 11:24:54 -0300

    add authForm component

---
+++ components/auth/authForm.tsx
@@ -0,0 +1,38 @@
+import { UseFormRegister } from 'react-hook-form';
+import { Stack } from '@chakra-ui/react';
+
+import { InputComponent } from '../inputs/Input';
+import { SubmitButton } from '../buttons/SubmitButton';
+
+import { Inputs } from '@/types/Form/inputs';
+
+interface Props {
+  onSubmit: () => void;
+  isLoading: boolean;
+  register: UseFormRegister<Inputs>;
+  errors: any;
+}
+
+export const AuthForm = ({ onSubmit, isLoading, errors, register }: Props) => {
+  return (
+    <Stack spacing={4}>
+      <InputComponent
+        register={register}
+        errors={!!errors.username}
+        warning="El usuario es obligatorio."
+        title={'Usuario'}
+        type={'text'}
+        id={'username'}
+      />
+      <InputComponent
+        register={register}
+        errors={!!errors.password}
+        warning="La contraseña es obligatoria"
+        title={'Contraseña'}
+        type={'password'}
+        id={'password'}
+      />
+      <SubmitButton onSubmit={onSubmit} isLoading={isLoading} />
+    </Stack>
+  );
+};
