a09e2ad6dba2d43466e895879034eef4c7408950
Leonardo <leonardo38361@gmail.com>
2024-02-02 20:03:08 -0300

    Types for Chakra UI icons

---
+++ types/iconName/iconName.ts
@@ -0,0 +1,17 @@
+export type IconType =
+  | 'AiFillCar'
+  | 'AiFillCalendar'
+  | 'AiFillPushpin'
+  | 'RiHistoryLine'
+  | 'RiTodoFill'
+  | 'RiDashboardFill'
+  | 'RiRoadMapLine'
+  | 'BsCardList'
+  | 'BsFillPersonLinesFill'
+  | 'BsFillTicketFill'
+  | 'BsJournals'
+  | 'BsSearch'
+  | 'BsTelephoneFill'
+  | 'BsTelephonePlusFill'
+  | 'IoMapSharp'
+  | 'GiPapers';
