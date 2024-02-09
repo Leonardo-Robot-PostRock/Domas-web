a612a14a6d1f71a155e71b9868ff9032ee93d1c7
Leonardo <leonardo38361@gmail.com>
2024-02-09 04:48:30 -0300

    AxiosError function deleted

--- utils/AxiosError.ts
+++
@@ -1,8 +0,0 @@
-import { AxiosError } from 'axios';
-
-export default function isAxiosError(candidate: unknown): candidate is AxiosError {
-  if (candidate && typeof candidate === 'object' && 'isAxiosError' in candidate) {
-    return true;
-  }
-  return false;
-}
