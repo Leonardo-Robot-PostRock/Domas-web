0c050d89c1e279bdc3a68067ba4fbfff64663421
Leonardo <leonardo38361@gmail.com>
2024-02-15 09:47:07 -0300

    new reducer added

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import teamsReducer from './teams/teamsReducer';
 import formReducer from './formData/formReducer';
+import squadSlice from './squad/squadReducer';
 
 export const store = configureStore({
   reducer: {
     teams: teamsReducer,
+    squad: squadSlice,
     form: formReducer,
   },
 });
@@ -13,3 +16,7 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
