667b6084220d340d7c309d3506c0d0ca71ea42c3
Leonardo <leonardo38361@gmail.com>
2024-02-15 09:31:33 -0300

    add types and handleSave method fixed

--- src/hooks/tableTeams/useHandleDrawer.ts
+++ src/hooks/tableTeams/useHandleDrawer.ts
@@ -4,6 +4,7 @@ import { findNodeById, insertNode } from '@table-library/react-table-library/com
 import { TableNode } from '@table-library/react-table-library/types/table';
 import { useAppDispatch, useAppSelector } from '@/store';
 import { setData, setSquadDrawerId, setSquadEdited } from '@/store/squad/squadReducer';
+import { ChangeEvent } from 'react';
 
 /* Drawer */
 export const useHandleDrawer = () => {
@@ -13,7 +14,7 @@ export const useHandleDrawer = () => {
   const drawerId = useAppSelector((state) => state.squad.drawerId);
   const edited = useAppSelector((state) => state.squad.edited);
 
-  const handleEdit = (event) => {
+  const handleEdit = (event: ChangeEvent<HTMLInputElement>) => {
     console.log(event);
 
     setSquadEdited(event.target.value);
@@ -25,9 +26,9 @@ export const useHandleDrawer = () => {
   };
 
   const handleSave = () => {
-    const node = findNodeById(data, drawerId as string | number);
+    const node = findNodeById(data.nodes, drawerId as string | number);
     const editedNode: EditedNode = { ...node, name: edited };
-    const nodes = insertNode(data, editedNode as TableNode);
+    const nodes = insertNode(data.nodes, editedNode as TableNode);
 
     dispatch(setData({ nodes }));
 
