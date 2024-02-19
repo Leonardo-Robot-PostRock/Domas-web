7c5419a97bd1b1182f7b7b0c1a51c5f10a233551
Leonardo <leonardo38361@gmail.com>
2024-02-19 13:53:06 -0300

    [IMPROVE] asynchronous logic separation

--- src/app/(dashboard)/teams/page.tsx
+++ src/app/(dashboard)/teams/page.tsx
@@ -1,51 +1,27 @@
 'use client';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '@/lib';
 
 import { Box, Text } from '@chakra-ui/react';
 
 import { useTheme } from '@table-library/react-table-library/theme';
 import { getTheme } from '@table-library/react-table-library/baseline';
 
-import axios from 'axios';
-
 import { toastError } from '@/components/toast/toastError';
 import { Table } from '@/components/ui/table/Table';
-import { Team } from '@/types/api/teamById';
-import { useAppDispatch, useAppSelector } from '@/lib';
-import { setSquadNodes } from '@/lib/store/squad/squadTableReducer';
 import { ModalProvider } from '@/context/ModalProvider';
+import { fetchTeams } from '@/lib/store/teamsTable/thunks';
 
 export default function Teams() {
   const dispatch = useAppDispatch();
-  const nodes = useAppSelector((state) => state.squadTable.nodes);
-
-  const [userInfo, setUserInfo] = useState();
+  const teams = useAppSelector((state) => state.teamsTable.teams);
 
   useEffect(() => {
-    const user = JSON.parse(localStorage.getItem('user') as string);
+    dispatch(fetchTeams());
+  }, [dispatch]);
 
-    setUserInfo(user);
-  }, []);
-
-  const teamsAll = '/api/teams/all';
-
-  const fetchData = useCallback(async () => {
-    try {
-      const result: Team = await axios.get(teamsAll);
-
-      dispatch(setSquadNodes(result.data));
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
-
-  useEffect(() => {
-    fetchData();
-  }, [fetchData]);
-
-  if (!nodes.length) {
-    const errorProps = 'Ocurrió un error al intentar mostrar las cuadrillas.';
-    toastError(errorProps);
+  if (!teams.length) {
+    toastError('Ocurrió un error al intentar mostrar las cuadrillas.');
     return null;
   }
 
