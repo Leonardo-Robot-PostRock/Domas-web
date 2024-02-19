782b72e6518828e4e2884b0f3afa9b23f1771d5e
Leonardo <leonardo38361@gmail.com>
2024-02-19 14:03:25 -0300

    [DELETED] interface deleted

--- types/store/teamsData.ts
+++ types/store/teamsData.ts
@@ -1,14 +1,7 @@
-import { FilePondProps } from 'react-filepond';
+import { TeamEdit } from '../Form/teamEdit';
 
 export interface TeamsData {
-  showSupervisorField: boolean;
-  supervisorsDataField: { value: number; label: string }[];
-  technicianDataField: { value: number; label: string }[];
-  primaryFile: FilePondProps[];
-  secondaryFile: FilePondProps[];
-  clusterData: { value: number; label: string }[];
-  areaData: { value: number; label: string }[];
-  clustersGroup: { value: number; label: string }[];
-  areaGroup: { value: number; label: string }[];
-  clustersFav: { cluster_id: number; isChecked: boolean }[];
+  teamData?: TeamEdit | null;
+  loading: boolean;
+  error: Error | null;
 }
