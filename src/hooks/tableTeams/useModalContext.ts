ffea462cd3e6eead89fbdf56d9f5c6cd2957985a
Leonardo <leonardo38361@gmail.com>
2024-02-19 13:41:14 -0300

    [CREATE] useModalContext hook

---
+++ src/hooks/tableTeams/useModalContext.ts
@@ -0,0 +1,11 @@
+import { ModalContext } from '@/context/ModalContext';
+import { useContext } from 'react';
+
+export const useModalContext = () => {
+  const context = useContext(ModalContext);
+
+  if (!context) {
+    throw new Error('useModalContext must be used within a ModalProvider');
+  }
+  return context;
+};
