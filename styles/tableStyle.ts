4880119b6b878e0b288d3eba663ff720c38ce267
Leonardo <leonardo38361@gmail.com>
2024-02-20 23:42:02 -0300

    styles for the table

--- styles/tableStyle.ts
+++ styles/tableStyle.ts
@@ -5,11 +5,22 @@ const mantineTheme = getTheme({
   ...DEFAULT_OPTIONS,
   striped: false,
 });
+
 const customTheme = {
   Table: `
-      --data-table-library_grid-template-columns:  44px repeat(5, minmax(0, 1fr));
-      margin: 20px;
+    grid-template-columns: 44px repeat(11, minmax(0, 1fr));
+    margin: 20px 20px 0 20px;
+    place-items: center;
+    
+    @media(min-width: 902px) {
       place-items: center;
+      grid-auto-rows: minmax(max-content, 60px)
+    }
+    
+    @media(min-width: 1440px) {
+      place-items: center;
+      grid-auto-rows: minmax(max-content, 70px)
+    }
   `,
   HeaderCell: `
     width:100%;
@@ -25,16 +36,19 @@ const customTheme = {
       font-size: 10px;
       white-space: normal;
     }  
+
+    .th * {
+      justify-content: center;
+    }
+
+    .th:first-of-type, th:first-of-type > div{
+      width: 24px;
+    }
   `,
   Cell: `
     font-size: 11px;
     width: 100%;
-    white-space: normal;
-    
-    div {
-      text-align: center;
-      white-space: normal;
-    }
+    text-align:center;
   `,
 
   Row: `
@@ -46,6 +60,14 @@ const customTheme = {
     &:nth-of-type(even) {
       background-color: #EEF1FF;
       color: #25316D;
+    }
+
+    .td:first-of-type > div {
+      margin-left: 10px
+    }
+
+    .td:first-of-type, div {
+      padding: 0;
     }
   `,
 };
