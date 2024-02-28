c0953baf8c532b2a54da8952d17309f869ef82fa
Leonardo <leonardo38361@gmail.com>
2024-02-28 00:39:22 -0300

    [ADD] typing

--- src/app/api/cluster/route.ts
+++ src/app/api/cluster/route.ts
@@ -1,7 +1,7 @@
 import axios, { isAxiosError } from 'axios';
 import { redirect } from 'next/navigation';
 
-export async function GET(request: { cookies: { get: (arg0: string) => any } }) {
+export async function GET(request: { cookies: { get: (arg0: string) => any } }): Promise<Response | undefined> {
   const token = request.cookies.get('auth_service');
 
   if (!token) return redirect(`${process.env.APP_URL}/login`);
@@ -11,19 +11,19 @@ export async function GET(request: { cookies: { get: (arg0: string) => any } }) 
   try {
     const clusterResponse = await axios.get(clusterUrl, {
       headers: {
-        Cookie: `auth_service=${token.value}`,
-      },
+        Cookie: `auth_service=${token.value}`
+      }
     });
 
-    return new Response(JSON.stringify(clusterResponse.data), {
+    return new Response(JSON.stringify(clusterResponse.data.data), {
       status: 200,
-      headers: { 'Content-Type': 'application/json' },
+      headers: { 'Content-Type': 'application/json' }
     });
   } catch (error) {
     if (isAxiosError(error)) {
-      return new Response(JSON.stringify({ error: true, message: error.toString() }), {
+      return new Response(JSON.stringify({ error: true, message: error }), {
         status: 400,
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 'Content-Type': 'application/json' }
       });
     }
   }
--- src/app/api/teams/[id]/route.ts
+++ src/app/api/teams/[id]/route.ts
@@ -2,7 +2,7 @@ import { redirect } from 'next/navigation';
 import axios, { isAxiosError } from 'axios';
 import sharp from 'sharp';
 
-import { RequestCookies } from 'next/dist/compiled/@edge-runtime/cookies';
+import { type RequestCookies } from 'next/dist/compiled/@edge-runtime/cookies';
 
 interface GetParams {
   id: string;
@@ -16,7 +16,7 @@ interface RequestObject {
   get: (arg0: string) => any;
 }
 
-export async function GET(request: RequestObject, { params }: { params: GetParams }) {
+export async function GET(request: RequestObject, { params }: { params: GetParams }): Promise<Response | undefined> {
   const token = request.cookies.get('auth_service');
 
   if (!token) return redirect(`${process.env.APP_URL}/login`);
@@ -31,8 +31,8 @@ export async function GET(request: RequestObject, { params }: { params: GetParam
   try {
     const teamsResponse = await axios.get(url, {
       headers: {
-        Cookie: `auth_service=${token.value}`,
-      },
+        Cookie: `auth_service=${token.value}`
+      }
     });
 
     console.log(teamsResponse.data);
@@ -40,26 +40,26 @@ export async function GET(request: RequestObject, { params }: { params: GetParam
     return new Response(JSON.stringify(teamsResponse?.data), {
       status: 200,
       headers: {
-        'Content-Type': 'application/json',
-      },
+        'Content-Type': 'application/json'
+      }
     });
   } catch (error: unknown) {
     if (isAxiosError(error)) {
       if (error.response) {
-        return new Response(JSON.stringify({ error: true, message: error.toString() }), {
+        return new Response(JSON.stringify({ error: true, message: error.message }), {
           status: 400,
-          headers: { 'Content-Type': 'application/json' },
+          headers: { 'Content-Type': 'application/json' }
         });
       }
-      return new Response(JSON.stringify({ error: true, message: error.toString() }), {
+      return new Response(JSON.stringify({ error: true, message: error.message }), {
         status: 500,
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 'Content-Type': 'application/json' }
       });
     }
   }
 }
 
-export async function PATCH(request: RequestObject, { params }: { params: GetParams }) {
+export async function PATCH(request: RequestObject, { params }: { params: GetParams }): Promise<Response | undefined> {
   const token = request.cookies.get('auth_service');
 
   if (!token) return redirect(`${process.env.APP_URL}/login`);
@@ -70,43 +70,47 @@ export async function PATCH(request: RequestObject, { params }: { params: GetPar
 
   if (request.body.primary_file) {
     const file = request.body.primary_file;
-    var img = Buffer.from(file, 'base64');
-    const data = await sharp(img).resize({ width: 200, height: 200 }).toFormat('jpeg').toBuffer();
+    if (typeof file === 'string') {
+      const img = Buffer.from(file, 'base64');
+      const data = await sharp(img).resize({ width: 200, height: 200 }).toFormat('jpeg').toBuffer();
 
-    request.body.primary_file = data.toString('base64');
+      request.body.primary_file = data.toString('base64');
+    }
   }
   if (request.body.secondary_file) {
     const file = request.body.secondary_file;
-    var img = Buffer.from(file, 'base64');
-    const data = await sharp(img).resize({ width: 200, height: 200 }).toFormat('jpeg').toBuffer();
+    if (typeof file === 'string') {
+      const img = Buffer.from(file, 'base64');
+      const data = await sharp(img).resize({ width: 200, height: 200 }).toFormat('jpeg').toBuffer();
 
-    request.body.secondary_file = data.toString('base64');
+      request.body.secondary_file = data.toString('base64');
+    }
   }
 
   try {
     const teamsResponse = await axios.patch(url, request.body, {
       headers: {
-        Cookie: `auth_service=${token.value}`,
-      },
+        Cookie: `auth_service=${token.value}`
+      }
     });
 
     return new Response(JSON.stringify(teamsResponse.data), {
       status: 200,
       headers: {
-        'Content-Type': 'application/json',
-      },
+        'Content-Type': 'application/json'
+      }
     });
   } catch (error: unknown) {
     if (isAxiosError(error)) {
-      return new Response(JSON.stringify({ error: true, message: error.toString() }), {
+      return new Response(JSON.stringify({ error: true, message: error.message }), {
         status: 400,
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 'Content-Type': 'application/json' }
       });
     }
   }
 }
 
-export async function DELETE(request: RequestObject, { params }: { params: GetParams }) {
+export async function DELETE(request: RequestObject, { params }: { params: GetParams }): Promise<Response | undefined> {
   const token = request.cookies.get('auth_service');
 
   if (!token) return redirect(`${process.env.APP_URL}/login`);
@@ -117,8 +121,8 @@ export async function DELETE(request: RequestObject, { params }: { params: GetPa
 
   const teamsResponse = await axios.delete(url, {
     headers: {
-      Cookie: `auth_service=${token.value}`,
-    },
+      Cookie: `auth_service=${token.value}`
+    }
   });
 
   console.log('TEAMS RESPONSE: ', teamsResponse.data);
@@ -127,14 +131,14 @@ export async function DELETE(request: RequestObject, { params }: { params: GetPa
     return new Response(JSON.stringify(teamsResponse.data), {
       status: 200,
       headers: {
-        'Content-Type': 'application/json',
-      },
+        'Content-Type': 'application/json'
+      }
     });
   } catch (error: unknown) {
     if (isAxiosError(error)) {
-      return new Response(JSON.stringify({ error: true, message: error.toString() }), {
+      return new Response(JSON.stringify({ error: true, message: error.message }), {
         status: 400,
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 'Content-Type': 'application/json' }
       });
     }
   }
