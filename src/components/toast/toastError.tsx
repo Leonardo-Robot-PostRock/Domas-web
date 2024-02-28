1c098b11760cd88137d1b65a0732fc44b8cb2fe4
Leonardo <leonardo38361@gmail.com>
2024-02-28 00:21:58 -0300

    [UPDATE] change return typing

--- src/components/toast/toastError.tsx
+++ src/components/toast/toastError.tsx
@@ -1,8 +1,6 @@
-import type { ReactNode } from 'react';
-import type { Message } from 'react-hook-form';
 import { type ToastPosition, toast } from 'react-hot-toast';
 
-export const toastError = (message: Message, position = 'top'): ReactNode =>
+export const toastError = (message: string, position = 'top'): void => {
   toast.error(message, {
     id: message,
     duration: 5000,
@@ -14,3 +12,6 @@ export const toastError = (message: Message, position = 'top'): ReactNode =>
       boxShadow: '0px 2px 22px 0px rgba(235,78,44,0.20)'
     }
   });
+};
+
+toastError('Ocurrió un error al cargar los cluster');
