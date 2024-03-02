38d90164f5a2c7eacbdf5a28f92b549e078a0b15
Leonardo <leonardo38361@gmail.com>
2024-03-02 04:27:55 -0300

    [UPDATE] typing improvement and data sending is corrected

--- src/app/api/area/route.ts
+++ src/app/api/area/route.ts
@@ -1,8 +1,9 @@
-import { Area } from '@/types/api/area';
+import type { AreaData } from '@/types/api/area';
+import type { RequestObject } from '@/types/api/request';
 import axios, { isAxiosError } from 'axios';
 import { redirect } from 'next/navigation';
 
-export async function GET(request: { cookies: { get: (arg0: string) => any } }) {
+export async function GET(request: RequestObject): Promise<Response | undefined> {
   const token = request.cookies.get('auth_service');
 
   if (!token) return redirect(`${process.env.APP_URL}/login`);
@@ -10,21 +11,21 @@ export async function GET(request: { cookies: { get: (arg0: string) => any } }) 
   const areaUrl = `${process.env.AUTH_BASE_URL}/v1/areas`;
 
   try {
-    const areaResponse = await axios.get<Area>(areaUrl, {
+    const areaResponse = await axios.get<AreaData>(areaUrl, {
       headers: {
-        Cookie: `auth_service=${token.value}`,
-      },
+        Cookie: `auth_service=${token.value}`
+      }
     });
 
-    return new Response(JSON.stringify(areaResponse.data), {
+    return new Response(JSON.stringify(areaResponse.data.data), {
       status: 200,
-      headers: { 'Content-Type': 'application/json' },
+      headers: { 'Content-Type': 'application/json' }
     });
   } catch (error) {
     if (isAxiosError(error)) {
-      return new Response(JSON.stringify({ error: true, message: error.toString() }), {
+      return new Response(JSON.stringify({ error: true, message: error.message }), {
         status: 400,
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 'Content-Type': 'application/json' }
       });
     }
   }
