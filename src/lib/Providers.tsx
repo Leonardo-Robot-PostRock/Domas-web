dbc0f2c5bb12d23d4d5a262342b270654b633097
Leonardo <leonardo38361@gmail.com>
2024-03-02 05:02:43 -0300

    [ADD] standard configuration

---
+++ src/lib/Providers.tsx
@@ -0,0 +1,8 @@
+'use client';
+import React, { type ReactNode } from 'react';
+import { Provider } from 'react-redux';
+import { store } from '.';
+
+export const Providers = ({ children }: { children: ReactNode }): ReactNode => {
+  return <Provider store={store}>{children}</Provider>;
+};
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { type TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 // Slices
 import areaSlice from './store/area/areaSlice';
@@ -10,26 +10,22 @@ import teamsSlice from './store/teams/teamsSlice';
 import teamsTableSlice from './store/teamsTable/teamsTableSlice';
 import techniciansSlice from './store/technicians/techniciansSlice';
 
-export const store = () => {
-  return configureStore({
-    reducer: {
-      area: areaSlice,
-      cluster: clusterSlice,
-      form: formSlice,
-      supervisor: supervisorSlice,
-      teams: teamsSlice,
-      teamsTable: teamsTableSlice,
-      technicians: techniciansSlice,
-    },
-  });
-};
+export const store = configureStore({
+  reducer: {
+    area: areaSlice,
+    cluster: clusterSlice,
+    form: formSlice,
+    supervisor: supervisorSlice,
+    teams: teamsSlice,
+    teamsTable: teamsTableSlice,
+    technicians: techniciansSlice
+  }
+});
 
-// Infer the type of store
-export type AppStore = ReturnType<typeof store>;
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<AppStore['getState']>;
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = AppStore['dispatch'];
+export type AppDispatch = typeof store.dispatch;
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch;
