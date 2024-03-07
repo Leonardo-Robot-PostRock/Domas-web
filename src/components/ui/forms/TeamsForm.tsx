0eaf5309bf1e27b0a0cdeab7f091c94cb2bb4dd6
Leonardo <leonardo38361@gmail.com>
2024-03-07 04:08:25 -0300

    component optimization

--- src/components/ui/forms/TeamsForm.tsx
+++ src/components/ui/forms/TeamsForm.tsx
@@ -1,30 +1,45 @@
-import { useEffect, type ReactNode } from 'react';
+import { useEffect, type ReactNode, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '@/lib';
 import { setAreaGroup } from '@/lib/store/area/areaSlice';
 
-import { useDataInitialization } from '@/hooks/form/useDataInitialization';
-import { useFormData } from '@/hooks/form/useFormData';
-import { useFetchFormData } from '@/hooks/form/useFetchFormData';
-import { useFormSubmit } from '@/hooks/form/useFormSubmit';
+import { useDataInitialization, useFetchFormData, useFormData, useFormSubmit } from '@/hooks/form/';
+import { useModalContext } from '@/hooks/tableTeams/useModalContext';
 
 import { FilepondComponent } from '../../FilepondComponent/FilepondComponent';
-import { FormField } from '@/components/formField/fields/FormField';
-import { FormFieldLayout } from '@/components/formField/FormFieldLayout';
-import { FormFieldTickets } from '@/components/formField/fields/FormFieldTickets';
-import { SelectField } from '@/components/formField/fields/SelectField';
-import { FormValidations } from '@/utils/TeamsFormUtils';
-import { Label } from '@/components/label/Label';
-import { SelectFieldCluster } from '../../formField/fields/SelectFieldCluster';
-import { SelectFieldMainCluster } from '@/components/formField/fields/SelectFieldMainCluster';
+import {
+  FormField,
+  FormFieldTickets,
+  SelectField,
+  SelectFieldCluster,
+  CheckboxMainCluster,
+  FormFieldStartingPoint,
+  FormFieldLayout
+} from '@/components/formField/';
+import { Flex, Text } from '@chakra-ui/react';
+import type { FilePondFile } from 'filepond';
+import { SubmitButton } from '@/components/buttons/SubmitButton';
+import { BackButton } from '@/components/buttons/BackButton';
+import { generateFormFileField, genericFormFields, ticketFormFields } from '@/utils/formTeams/';
 
 export const TeamsForm = (): ReactNode => {
   const dispatch = useAppDispatch();
   const { area, areaGroup } = useAppSelector((state) => state.area);
-  const { primaryFile, secondaryFile } = useAppSelector((state) => state.teams);
   const { supervisorsDataField, showSupervisorField } = useAppSelector((state) => state.supervisor);
-
+  const { technicianDataField } = useAppSelector((state) => state.technicians);
+  // useState for handle images files
+  const [primaryFile, setPrimaryFile] = useState<FilePondFile[]>([]);
+  const [secondaryFile, setSecondaryFile] = useState<FilePondFile[]>([]);
+  // Array for files field
+  const formFileField = generateFormFileField(primaryFile, setPrimaryFile, secondaryFile, setSecondaryFile);
+  // Custom hooks to manage method onSubmit, todo fetch to the api/teams, initialize data and to handle the modal form.
   const teamEdit = useDataInitialization();
-  const onSubmit = useFormSubmit();
+  const { fetchData } = useFetchFormData();
+  const { onClose } = useModalContext();
+  const onSubmit = useFormSubmit(primaryFile, secondaryFile);
+
+  useEffect(() => {
+    void fetchData();
+  }, [fetchData]);
 
   const {
     register,
@@ -39,81 +54,86 @@ export const TeamsForm = (): ReactNode => {
     }
   };
 
-  const { fetchData } = useFetchFormData();
-
-  useEffect(() => {
-    void fetchData();
-  }, [dispatch, fetchData]);
-
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
       style={{ display: 'flex', flexDirection: 'column', gap: '20px', marginTop: '20px', width: '100%' }}
       onKeyDown={handleKeyDown}
     >
-      {/* Cuadrilla name */}
-      <FormField errors={errors} label="Nombre" name="name" register={register} validation={FormValidations.REQUIRED} />
-      {/* id google calendar */}
-      <FormField
-        errors={errors}
-        label="Google Calender ID"
-        name="google_calendar_id"
-        register={register}
-        validation={FormValidations.GOOGLE_CALENDAR_ID}
-      />
-      {/* Mesa username */}
-      <FormField
-        errors={errors}
-        label="Usuario de mesa"
-        name="mesa_username"
-        register={register}
-        validation={FormValidations.REQUIRED}
-      />
-      {/* Tickets form fields */}
-      <FormFieldTickets
-        errors={errors}
-        label="Mínimo de tickets a realizar"
-        name="min_tickets_to_do"
-        register={register}
-      />
-      <FormFieldTickets
-        errors={errors}
-        label="Máximo de tickets a realizar (Solo para OMNICANALIDAD)"
-        name="max_tickets_to_do_only_omnichannel"
-        register={register}
-      />
-      {/* Supervisor field */}
+      {/* ---- name, google calendar and mesa username fields  ---- */}
+      {genericFormFields.map((field) => (
+        <FormField
+          errors={errors}
+          key={field.id}
+          id={field.id}
+          label={field.label}
+          name={field.name}
+          register={register}
+          validation={field.validation}
+        />
+      ))}
+      {/* ---- Tickets form fields ---- */}
+      {ticketFormFields.map((field) => (
+        <FormFieldTickets
+          key={field.id}
+          id={field.id}
+          errors={errors}
+          label={field.label}
+          name={field.name}
+          register={register}
+        />
+      ))}
+      {/* ---- Supervisor field ---- */}
       {showSupervisorField && (
         <FormFieldLayout>
-          <Label>Supervisor*</Label>
+          <Text mb="10px">Supervisor*</Text>
           <SelectField control={control} name="supervisor" options={supervisorsDataField} rule={true} />
         </FormFieldLayout>
       )}
-      {/* Cluster field */}
+      {/* ---- Cluster field ---- */}
       <FormFieldLayout errors={errors} name="cluster">
-        <Label>Cluster*</Label>
+        <Text mb="10px">Cluster*</Text>
         <SelectFieldCluster control={control} errors={errors} />
       </FormFieldLayout>
-      {/* Main cluster checkbox */}
+      {/* ---- Main cluster checkbox ---- */}
       <FormFieldLayout>
-        <Label>Grupo principal del cluster</Label>
-        <SelectFieldMainCluster control={control} errors={errors} />
+        <Text mb="10px">Grupo principal del cluster</Text>
+        <CheckboxMainCluster control={control} errors={errors} />
       </FormFieldLayout>
-      {/* Area field */}
+      {/* ---- Area field ---- */}
       <FormFieldLayout errors={errors} name="area">
-        <Label>Area*</Label>
+        <Text mb="10px">Area*</Text>
         <SelectField
           control={control}
           name="area"
-          options={area}
           defaultValue={areaGroup}
-          onChange={(e) => setAreaGroup(e)}
+          options={area}
+          onChange={(e) => {
+            dispatch(setAreaGroup(e));
+          }}
           rule={!(areaGroup.length > 0)}
         />
       </FormFieldLayout>
-      {/* Upload photos */}
-      <FilepondComponent file={primaryFile} title="Foto del lider" />
-      <FilepondComponent file={secondaryFile} title="Foto del técnico asistente" />
+      {/* ---- Starting point field ---- */}
+      <FormFieldStartingPoint
+        id="startingPointField"
+        errors={errors}
+        label="Punto de partida*"
+        name="starting_point"
+        register={register}
+      />
+      {/* ---- Select leader, assistant fields and upload photos with FilePond ---- */}
+      {formFileField.map((data) => (
+        <FormFieldLayout key={data.title} errors={errors} name={data.name}>
+          <Text mb="10px">{data.label}</Text>
+          <SelectField control={control} name={data.name} options={technicianDataField} rule={true} />
+          <FilepondComponent file={data.file} title={data.title} setFile={data.setFile} />
+        </FormFieldLayout>
+      ))}
+      <Flex justifyContent="center" gap={4}>
+        <BackButton onClose={onClose} title="Volver" />
+        <SubmitButton title={teamEdit ? 'Actualizar cuadrilla' : 'Crear nueva cuadrilla'} />
+      </Flex>
     </form>
   );
 };
