976ac8d8b4610d67d7b094a65627608872cf79ac
Leonardo <leonardo38361@gmail.com>
2024-03-11 21:26:08 -0300

    [UPDATED] usage of react-toastify

--- utils/errorHandling.ts
+++ utils/errorHandling.ts
@@ -1,11 +1,11 @@
 import type { Dispatch } from 'redux';
 import { isAxiosError } from 'axios';
-import { toastError } from '@/components/toast';
 import { setError } from '@/lib/store/teams/teamsSlice';
+import { toast } from 'react-toastify';
 
 export const handleAxiosError = (dispatch: Dispatch, error: any): void => {
   if (isAxiosError(error)) {
     dispatch(setError(error.message));
-    toastError('Ocurrió un error durante la operación');
+    toast.error(error.message);
   }
 };
