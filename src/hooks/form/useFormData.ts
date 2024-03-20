4d1e8948b3f7cf0b03270971b9ac68d364641089
Leonardo <leonardo38361@gmail.com>
2024-03-20 02:12:41 -0300

    [ADD] using the hook for default values

--- src/hooks/form/useFormData.ts
+++ src/hooks/form/useFormData.ts
@@ -1,10 +1,12 @@
 import type { TeamEdit } from '@/types/Form/teamEdit';
-import { getDefaultValues } from '@/utils/formTeams/formDefaultValues';
 import type { FieldValues } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
+import { useFormDefaultValues } from './useFormDefaultValues';
 
 export const useFormData = (teamEdit: TeamEdit | null | undefined): FieldValues => {
+  const defaultFormValues = useFormDefaultValues(teamEdit);
+
   return useForm({
-    defaultValues: teamEdit ? getDefaultValues(teamEdit) : {}
+    defaultValues: teamEdit ? defaultFormValues : {}
   });
 };
